1bf0307387fa203eb4c603d98e32e411a0edc2b4
OdeeyuhLog <charlesfreud405@protonmail.com>
2025-10-26 03:08:41 +0800

    Add controller

---
+++ csharp/MyFabulousApi/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using MyFabulousApi.Models;
+
+namespace MyFabulousApi.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class UserController : ControllerBase
+  {
+  }
+}
